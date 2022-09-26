f8b1cfd197e4f0a8b881309f972198c365d31432
Dmitry A <adcntt@gmail.com>
2022-09-26 03:05:32 +0300

    mixin and mobile-first

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,10 @@ const sass = require('gulp-sass')(require('sass'));
 const prefix = require('gulp-autoprefixer');
 
 function compileScss() {
-  return src('src/scss/*scss').pipe(sass()).pipe(prefix('last 2 versions')).pipe(dest('./dist/css'));
+  return src('src/scss/*scss') //
+    .pipe(sass())
+    .pipe(prefix('last 2 versions'))
+    .pipe(dest('./dist/css'));
 }
 
 function watchTask() {
